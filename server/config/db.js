37d492392240c8bf9d798b0f7bbe267e94b7235f
nalina <naleenameshram85@gmail.com>
2026-06-06 12:15:20 +0530

    working

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGO_URI);
+    const dburl = await mongoose.connect(process.env.MONGO_URI);
+    const db = mongoose.connection;
+    console.log(`MongoDB connected:${dburl.connection.host}`);
+    console.log(`mongodb connected successfully...`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+export default connectDB;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 const app = express();
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.MONGO_URI);
+//connect to the database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!!");
   res.end();
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
